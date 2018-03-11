35a67df0dff60d07ca35547d2f94c027825da0b3
synend16 <synend16@student.westerdals.no>
2018-03-11 14:56:43 +0100

    Skal nå erstatte min front-end-package med Andrea sin

--- src/test/java/com/endre/java/springquizgame/selenium/SeleniumLocalIT.java
+++ src/test/java/com/endre/java/springquizgame/selenium/SeleniumLocalIT.java
@@ -14,10 +14,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class,
-webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+        webEnvironment = RANDOM_PORT)
 public class SeleniumLocalIT {
 
     private static WebDriver driver;
@@ -47,7 +48,7 @@ public class SeleniumLocalIT {
     private IndexPO home;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         driver.manage().deleteAllCookies();
 
         home = new IndexPO(driver, "localhost", port);
