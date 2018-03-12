a97102c9ff98d6ff6546dc64c1c7decdd3b9e14f
synend16 <synend16@student.westerdals.no>
2018-03-12 13:32:58 +0100

    Funker fortsatt ikke, skal prøve å klone på ntt

--- src/main/java/com/endre/java/springquizgame/controller/RedirectForwardHandler.java
+++ src/main/java/com/endre/java/springquizgame/controller/RedirectForwardHandler.java
@@ -1,13 +1,14 @@
 package com.endre.java.springquizgame.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class RedirectForwardHandler {
 
-    @GetMapping(value = "/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String forward(){
         return "forward:index.xhtml";
     }
