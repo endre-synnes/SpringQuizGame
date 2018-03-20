432ef64c4c2cda48e0b4962e80aa489e8c2689c1
synend16 <synend16@student.westerdals.no>
2018-03-20 21:59:08 +0100

    Begynt på flyttingen

---
+++ backend/src/main/java/com/endre/java/springquizgame/backend/service/CategoryService.java
@@ -0,0 +1,75 @@
+package com.endre.java.springquizgame.backend.service;
+
+import com.endre.java.springquizgame.entity.Category;
+import com.endre.java.springquizgame.entity.SubCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Service
+@Transactional
+public class CategoryService {
+
+    @Autowired
+    private EntityManager em;
+
+    public Long createCategory(String name){
+        Category category = new Category();
+        category.setName(name);
+
+        em.persist(category);
+        return category.getId();
+    }
+
+
+    public Long createSubCategory(Long parentId, String name) {
+
+        Category category = em.find(Category.class, parentId);
+        if (category == null){
+            throw new IllegalArgumentException("Could not find matching parent Category");
+        }
+        SubCategory subCategory = new SubCategory();
+        subCategory.setParent(category);
+        subCategory.setName(name);
+
+        category.getSubCategories().add(subCategory);
+
+        em.persist(subCategory);
+
+        return subCategory.getId();
+    }
+
+
+    // Specify if you want to force load the subCategory of all categories
+    public List<Category> getAllCategories(boolean withSub){
+        TypedQuery<Category> query = em.createQuery("select c from Category c", Category.class);
+        List<Category> categories = query.getResultList();
+
+        if (withSub){
+            //force loading
+            categories.forEach(c -> c.getSubCategories().size());
+        }
+
+        return categories;
+    }
+
+
+    public Category getCategory(long id, boolean withSub){
+        Category category = em.find(Category.class, id);
+        if (withSub && category != null){
+            category.getSubCategories().size();
+        }
+        return category;
+    }
+
+
+    public SubCategory getSubcategory(long id){
+        return em.find(SubCategory.class, id);
+    }
+
+
+}
---
+++ backend/src/main/java/com/endre/java/springquizgame/backend/service/QuizService.java
@@ -0,0 +1,94 @@
+package com.endre.java.springquizgame.backend.service;
+
+import com.endre.java.springquizgame.entity.Quiz;
+import com.endre.java.springquizgame.entity.SubCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.*;
+
+@Service
+@Transactional
+public class QuizService {
+
+    @Autowired
+    private EntityManager em;
+
+    public long createQuiz(long subCategoryId, String question, String firstAnswer,
+                           String secondAnswer, String thirdAnswer, String fourthAnswer,
+                           int indexOfCorrectAnswer){
+
+        SubCategory subCategory = em.find(SubCategory.class, subCategoryId);
+        if (subCategory == null){
+            throw new IllegalArgumentException("Could not find matching Subcategory!");
+        }
+
+
+        Quiz quiz = new Quiz();
+        quiz.setQuestion(question);
+        quiz.setFirstAnswer(firstAnswer);
+        quiz.setSecondAnswer(secondAnswer);
+        quiz.setThirdAnswer(thirdAnswer);
+        quiz.setFourthAnswer(fourthAnswer);
+        quiz.setIndexOfCorrectAnswer(indexOfCorrectAnswer);
+        quiz.setSubCategory(subCategory);
+
+        em.persist(quiz);
+
+        return quiz.getId();
+    }
+
+
+    public List<Quiz> getQuizzes(){
+        TypedQuery<Quiz> query = em.createQuery("select q from Quiz q", Quiz.class);
+        return query.getResultList();
+    }
+
+    public Quiz getQuiz(long id){
+        Quiz quiz = em.find(Quiz.class, id);
+        if (quiz == null){
+            throw new IllegalArgumentException("Could not find Quiz matching this id");
+        }
+        return quiz;
+    }
+
+
+    public List<Quiz> getRandomQuizzes(int n, long categoryId) {
+
+        TypedQuery<Long> sizeQuery = em.createQuery(
+                "select count(q) from Quiz q where q.subCategory.parent.id=?1", Long.class);
+        sizeQuery.setParameter(1, categoryId);
+        long size = sizeQuery.getSingleResult();
+
+        if (n > size) {
+            throw new IllegalArgumentException("Cannot choose " + n + " unique quizzes out of the " + size + " existing");
+        }
+
+        Random random = new Random();
+
+        List<Quiz> quizzes = new ArrayList<>();
+        Set<Integer> chosen = new HashSet<>();
+
+        while (chosen.size() < n) {
+
+            int k = random.nextInt((int) size);
+            if (chosen.contains(k)) {
+                continue;
+            }
+            chosen.add(k);
+
+            TypedQuery<Quiz> query = em.createQuery(
+                    "select q from Quiz q where q.subCategory.parent.id=?1", Quiz.class);
+            query.setParameter(1, categoryId);
+            query.setMaxResults(1);
+            query.setFirstResult(k);
+
+            quizzes.add(query.getSingleResult());
+        }
+
+        return  quizzes;
+    }
+}
